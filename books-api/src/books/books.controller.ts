ca7c7265f626cdc16ddcb0130256476e3b40a05d
ivanab <ivana@opticaltone.com>
2021-11-24 15:33:37 +0100

    Added routes: findOneBook, deleteBook, updateBook

--- books-api/src/books/books.controller.ts
+++ books-api/src/books/books.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Delete,
+  Put,
+  Body,
+  Param,
+} from '@nestjs/common';
 import { CreateBookDto } from './dto/create-book.dto';
 
 @Controller('books')
@@ -8,9 +16,24 @@ export class BooksController {
     return 'Get all books!';
   }
 
+  @Get(':id')
+  findOneBook(@Param('id') id): string {
+    return `Book ${id}`;
+  }
+
   @Post()
   addNewBook(@Body() createBookDto: CreateBookDto): string {
     return `Title: ${createBookDto.title}, Author: ${createBookDto.author}, 
             Written in: ${createBookDto.year}, Number of Pages: ${createBookDto.pages}`;
+  }
+
+  @Delete(':id')
+  deleteBook(@Param('id') id): string {
+    return `Delete ${id}`;
+  }
+
+  @Put(':id')
+  updateBook(@Body() updateBookDto: CreateBookDto, @Param('id') id): string {
+    return `Update ${id}, Title: ${updateBookDto.title}`;
   }
 }
